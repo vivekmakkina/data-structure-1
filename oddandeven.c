10c16dc9dec6cf281ca6633c9ea58a97310780c9
vivekmakkina <112234444+vivekmakkina@users.noreply.github.com>
2022-09-23 08:06:57 +0530

    Add files via upload

---
+++ oddandeven.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int num;
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+    if (num % 2 == 0)
+    printf("the entered number is even");
+    else
+    print("the given number is odd");
+    return 0;
+}
+
+
